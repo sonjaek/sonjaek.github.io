dec93e23bddeb10307e0fb3ce9e41dbe6cbf859a
Sonja Ek <sonja.ek@sanoma.com>
2023-12-28 15:32:16 +0200

    Refactor language switcher

--- languageSwitcher.js
+++ languageSwitcher.js
@@ -3,17 +3,8 @@ $('[language="fi"]').hide()
 $('#lang-switch').change(function () {
   var language = $(this).val()
   $('[language]').hide()
-  switch (language) {
-    case 'en':
-      $('[language="en"]').show()
-      document.documentElement.lang = "en"
-      break
-    case 'fi':
-      $('[language="fi"]').show()
-      document.documentElement.lang = "fi"
-      break
-    default:
-      $('[language="en"]').show()
-      document.documentElement.lang = "en"
-  }
+  $('[language="' + language + '"]').show()
+  document.documentElement.lang = language
 })
+
+$('#lang-switch').change()
